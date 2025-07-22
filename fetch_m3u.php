5c0fcb240a87a8a6fd14cde67820e455f7b5d29c
koprulu <159997439+koprulu5564@users.noreply.github.com>
2025-07-23 01:00:15 +0300

    Create fetch_m3u.php

---
+++ fetch_m3u.php
@@ -0,0 +1,12 @@
+<?php
+$source_url = 'https://koprulu.liveblog365.com/kablotv.m3u';
+$output_file = 'kablotv_processed.m3u';
+
+// M3U içeriğini çek
+$m3u_content = file_get_contents($source_url);
+
+// Dosyaya yaz
+file_put_contents($output_file, $m3u_content);
+
+echo "M3U listesi başarıyla güncellendi!";
+?>
